a180455319a966c64239d88553d8925c251eaf22
PPahl04 <pamela.p00@hotmail.de>
2026-01-21 16:07:25 +0100

    Add project files.

---
+++ Window.Designer.cs
@@ -0,0 +1,394 @@
+﻿namespace AutoClicker
+{
+	partial class Window
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			components = new System.ComponentModel.Container();
+			btnStartStop = new Button();
+			lblInterval = new Label();
+			tmrClicker = new System.Windows.Forms.Timer(components);
+			nudInterval = new NumericUpDown();
+			tmrKeyDown = new System.Windows.Forms.Timer(components);
+			lblHint = new Label();
+			pnlUserTimeFrame = new Panel();
+			lblUserTimeFrame = new Label();
+			pnlInteractClicker = new Panel();
+			btnChangeWindowState = new Button();
+			tlpWindow = new TableLayoutPanel();
+			pnlStandardTimeFrame = new Panel();
+			lblStandardTimeFrame = new Label();
+			btn30sec = new Button();
+			btn1min = new Button();
+			btn5min = new Button();
+			pnlTimeCalculator = new Panel();
+			tbCalcResult = new TextBox();
+			nudInputTime = new NumericUpDown();
+			lblCalculate = new Label();
+			cbTimeUnit = new ComboBox();
+			((System.ComponentModel.ISupportInitialize)nudInterval).BeginInit();
+			pnlUserTimeFrame.SuspendLayout();
+			pnlInteractClicker.SuspendLayout();
+			tlpWindow.SuspendLayout();
+			pnlStandardTimeFrame.SuspendLayout();
+			pnlTimeCalculator.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)nudInputTime).BeginInit();
+			SuspendLayout();
+			// 
+			// btnStartStop
+			// 
+			btnStartStop.Anchor = AnchorStyles.None;
+			btnStartStop.AutoSize = true;
+			btnStartStop.Cursor = Cursors.Hand;
+			btnStartStop.Font = new Font("Bahnschrift", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			btnStartStop.Location = new Point(52, 4);
+			btnStartStop.Margin = new Padding(4, 3, 4, 3);
+			btnStartStop.Name = "btnStartStop";
+			btnStartStop.Size = new Size(163, 35);
+			btnStartStop.TabIndex = 0;
+			btnStartStop.Text = "Start Autoclicker";
+			btnStartStop.UseVisualStyleBackColor = true;
+			btnStartStop.Click += btnStartStop_Click;
+			// 
+			// lblInterval
+			// 
+			lblInterval.Anchor = AnchorStyles.None;
+			lblInterval.AutoSize = true;
+			lblInterval.Font = new Font("Bahnschrift", 11F);
+			lblInterval.Location = new Point(18, 35);
+			lblInterval.Margin = new Padding(4, 0, 4, 0);
+			lblInterval.Name = "lblInterval";
+			lblInterval.Size = new Size(97, 18);
+			lblInterval.TabIndex = 1;
+			lblInterval.Text = "Interval (ms):";
+			// 
+			// tmrClicker
+			// 
+			tmrClicker.Tick += timer_Tick;
+			// 
+			// nudInterval
+			// 
+			nudInterval.Anchor = AnchorStyles.None;
+			nudInterval.AutoSize = true;
+			nudInterval.Font = new Font("Bahnschrift", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			nudInterval.Location = new Point(128, 31);
+			nudInterval.Margin = new Padding(4, 3, 4, 3);
+			nudInterval.Maximum = new decimal(new int[] { 2400000, 0, 0, 0 });
+			nudInterval.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+			nudInterval.Name = "nudInterval";
+			nudInterval.Size = new Size(102, 26);
+			nudInterval.TabIndex = 2;
+			nudInterval.Tag = "interval";
+			nudInterval.Value = new decimal(new int[] { 60000, 0, 0, 0 });
+			nudInterval.ValueChanged += nudInput_ValueChanged;
+			nudInterval.KeyPress += nudInput_KeyPress;
+			// 
+			// tmrKeyDown
+			// 
+			tmrKeyDown.Enabled = true;
+			tmrKeyDown.Tick += tmrKeyDown_Tick;
+			// 
+			// lblHint
+			// 
+			lblHint.Anchor = AnchorStyles.None;
+			lblHint.AutoSize = true;
+			lblHint.Font = new Font("Bahnschrift", 11F);
+			lblHint.ForeColor = SystemColors.ControlDarkDark;
+			lblHint.Location = new Point(29, 42);
+			lblHint.Margin = new Padding(4, 0, 4, 0);
+			lblHint.Name = "lblHint";
+			lblHint.Size = new Size(209, 18);
+			lblHint.TabIndex = 3;
+			lblHint.Text = "Press Right Shift to start/stop";
+			// 
+			// pnlUserTimeFrame
+			// 
+			pnlUserTimeFrame.BorderStyle = BorderStyle.FixedSingle;
+			pnlUserTimeFrame.Controls.Add(nudInterval);
+			pnlUserTimeFrame.Controls.Add(lblUserTimeFrame);
+			pnlUserTimeFrame.Controls.Add(lblInterval);
+			pnlUserTimeFrame.Dock = DockStyle.Fill;
+			pnlUserTimeFrame.Location = new Point(4, 3);
+			pnlUserTimeFrame.Margin = new Padding(4, 3, 4, 3);
+			pnlUserTimeFrame.Name = "pnlUserTimeFrame";
+			pnlUserTimeFrame.Size = new Size(266, 72);
+			pnlUserTimeFrame.TabIndex = 4;
+			// 
+			// lblUserTimeFrame
+			// 
+			lblUserTimeFrame.Anchor = AnchorStyles.None;
+			lblUserTimeFrame.AutoSize = true;
+			lblUserTimeFrame.Font = new Font("Bahnschrift", 11F);
+			lblUserTimeFrame.Location = new Point(18, 10);
+			lblUserTimeFrame.Margin = new Padding(4, 0, 4, 0);
+			lblUserTimeFrame.Name = "lblUserTimeFrame";
+			lblUserTimeFrame.Size = new Size(173, 18);
+			lblUserTimeFrame.TabIndex = 3;
+			lblUserTimeFrame.Text = "User-defined time frame";
+			// 
+			// pnlInteractClicker
+			// 
+			pnlInteractClicker.BorderStyle = BorderStyle.FixedSingle;
+			pnlInteractClicker.Controls.Add(btnStartStop);
+			pnlInteractClicker.Controls.Add(lblHint);
+			pnlInteractClicker.Dock = DockStyle.Fill;
+			pnlInteractClicker.Location = new Point(4, 81);
+			pnlInteractClicker.Margin = new Padding(4, 3, 4, 3);
+			pnlInteractClicker.Name = "pnlInteractClicker";
+			pnlInteractClicker.Size = new Size(266, 73);
+			pnlInteractClicker.TabIndex = 11;
+			// 
+			// btnChangeWindowState
+			// 
+			btnChangeWindowState.Cursor = Cursors.Hand;
+			btnChangeWindowState.Dock = DockStyle.Right;
+			btnChangeWindowState.Font = new Font("Bahnschrift", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			btnChangeWindowState.Location = new Point(549, 0);
+			btnChangeWindowState.Margin = new Padding(4, 3, 4, 3);
+			btnChangeWindowState.Name = "btnChangeWindowState";
+			btnChangeWindowState.Size = new Size(21, 157);
+			btnChangeWindowState.TabIndex = 10;
+			btnChangeWindowState.Text = "<<";
+			btnChangeWindowState.UseVisualStyleBackColor = true;
+			btnChangeWindowState.Click += btnChangeWindowState_Click;
+			// 
+			// tlpWindow
+			// 
+			tlpWindow.AutoSize = true;
+			tlpWindow.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+			tlpWindow.ColumnCount = 2;
+			tlpWindow.ColumnStyles.Add(new ColumnStyle());
+			tlpWindow.ColumnStyles.Add(new ColumnStyle());
+			tlpWindow.Controls.Add(pnlUserTimeFrame, 0, 0);
+			tlpWindow.Controls.Add(pnlStandardTimeFrame, 1, 0);
+			tlpWindow.Controls.Add(pnlTimeCalculator, 1, 1);
+			tlpWindow.Controls.Add(pnlInteractClicker, 0, 1);
+			tlpWindow.Dock = DockStyle.Fill;
+			tlpWindow.Location = new Point(0, 0);
+			tlpWindow.Name = "tlpWindow";
+			tlpWindow.RowCount = 2;
+			tlpWindow.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+			tlpWindow.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+			tlpWindow.Size = new Size(549, 157);
+			tlpWindow.TabIndex = 4;
+			// 
+			// pnlStandardTimeFrame
+			// 
+			pnlStandardTimeFrame.BorderStyle = BorderStyle.FixedSingle;
+			pnlStandardTimeFrame.Controls.Add(lblStandardTimeFrame);
+			pnlStandardTimeFrame.Controls.Add(btn30sec);
+			pnlStandardTimeFrame.Controls.Add(btn1min);
+			pnlStandardTimeFrame.Controls.Add(btn5min);
+			pnlStandardTimeFrame.Dock = DockStyle.Fill;
+			pnlStandardTimeFrame.Location = new Point(278, 3);
+			pnlStandardTimeFrame.Margin = new Padding(4, 3, 4, 3);
+			pnlStandardTimeFrame.Name = "pnlStandardTimeFrame";
+			pnlStandardTimeFrame.Size = new Size(267, 72);
+			pnlStandardTimeFrame.TabIndex = 12;
+			// 
+			// lblStandardTimeFrame
+			// 
+			lblStandardTimeFrame.Anchor = AnchorStyles.None;
+			lblStandardTimeFrame.AutoSize = true;
+			lblStandardTimeFrame.Font = new Font("Bahnschrift", 11F);
+			lblStandardTimeFrame.Location = new Point(16, 6);
+			lblStandardTimeFrame.Margin = new Padding(4, 0, 4, 0);
+			lblStandardTimeFrame.Name = "lblStandardTimeFrame";
+			lblStandardTimeFrame.Size = new Size(142, 18);
+			lblStandardTimeFrame.TabIndex = 9;
+			lblStandardTimeFrame.Text = "Default time frames";
+			// 
+			// btn30sec
+			// 
+			btn30sec.Anchor = AnchorStyles.None;
+			btn30sec.AutoSize = true;
+			btn30sec.Cursor = Cursors.Hand;
+			btn30sec.Font = new Font("Bahnschrift", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			btn30sec.Location = new Point(16, 28);
+			btn30sec.Margin = new Padding(4, 3, 4, 3);
+			btn30sec.Name = "btn30sec";
+			btn30sec.Size = new Size(76, 35);
+			btn30sec.TabIndex = 5;
+			btn30sec.Text = "30 sec";
+			btn30sec.UseVisualStyleBackColor = true;
+			btn30sec.Click += btnStandardTimeFrame_Click;
+			// 
+			// btn1min
+			// 
+			btn1min.Anchor = AnchorStyles.None;
+			btn1min.AutoSize = true;
+			btn1min.Cursor = Cursors.Hand;
+			btn1min.Font = new Font("Bahnschrift", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			btn1min.Location = new Point(98, 28);
+			btn1min.Margin = new Padding(4, 3, 4, 3);
+			btn1min.Name = "btn1min";
+			btn1min.Size = new Size(76, 35);
+			btn1min.TabIndex = 6;
+			btn1min.Text = "1 min";
+			btn1min.UseVisualStyleBackColor = true;
+			btn1min.Click += btnStandardTimeFrame_Click;
+			// 
+			// btn5min
+			// 
+			btn5min.Anchor = AnchorStyles.None;
+			btn5min.AutoSize = true;
+			btn5min.Cursor = Cursors.Hand;
+			btn5min.Font = new Font("Bahnschrift", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			btn5min.Location = new Point(181, 28);
+			btn5min.Margin = new Padding(4, 3, 4, 3);
+			btn5min.Name = "btn5min";
+			btn5min.Size = new Size(76, 35);
+			btn5min.TabIndex = 7;
+			btn5min.Text = "5 min";
+			btn5min.UseVisualStyleBackColor = true;
+			btn5min.Click += btnStandardTimeFrame_Click;
+			// 
+			// pnlTimeCalculator
+			// 
+			pnlTimeCalculator.BorderStyle = BorderStyle.FixedSingle;
+			pnlTimeCalculator.Controls.Add(tbCalcResult);
+			pnlTimeCalculator.Controls.Add(nudInputTime);
+			pnlTimeCalculator.Controls.Add(lblCalculate);
+			pnlTimeCalculator.Controls.Add(cbTimeUnit);
+			pnlTimeCalculator.Dock = DockStyle.Fill;
+			pnlTimeCalculator.Location = new Point(278, 81);
+			pnlTimeCalculator.Margin = new Padding(4, 3, 4, 3);
+			pnlTimeCalculator.Name = "pnlTimeCalculator";
+			pnlTimeCalculator.Size = new Size(267, 73);
+			pnlTimeCalculator.TabIndex = 10;
+			// 
+			// tbCalcResult
+			// 
+			tbCalcResult.Anchor = AnchorStyles.None;
+			tbCalcResult.Font = new Font("Bahnschrift", 11.25F);
+			tbCalcResult.Location = new Point(99, 34);
+			tbCalcResult.Margin = new Padding(4, 3, 4, 3);
+			tbCalcResult.Name = "tbCalcResult";
+			tbCalcResult.ReadOnly = true;
+			tbCalcResult.Size = new Size(158, 26);
+			tbCalcResult.TabIndex = 9;
+			// 
+			// nudInputTime
+			// 
+			nudInputTime.Anchor = AnchorStyles.None;
+			nudInputTime.Font = new Font("Bahnschrift", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			nudInputTime.Location = new Point(16, 4);
+			nudInputTime.Margin = new Padding(4, 3, 4, 3);
+			nudInputTime.Maximum = new decimal(new int[] { 2400, 0, 0, 0 });
+			nudInputTime.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+			nudInputTime.Name = "nudInputTime";
+			nudInputTime.Size = new Size(71, 26);
+			nudInputTime.TabIndex = 3;
+			nudInputTime.Tag = "calculate";
+			nudInputTime.Value = new decimal(new int[] { 10, 0, 0, 0 });
+			nudInputTime.ValueChanged += nudInput_ValueChanged;
+			nudInputTime.KeyPress += nudInput_KeyPress;
+			// 
+			// lblCalculate
+			// 
+			lblCalculate.Anchor = AnchorStyles.None;
+			lblCalculate.AutoSize = true;
+			lblCalculate.Font = new Font("Bahnschrift", 11.25F);
+			lblCalculate.Location = new Point(16, 43);
+			lblCalculate.Margin = new Padding(4, 0, 4, 0);
+			lblCalculate.Name = "lblCalculate";
+			lblCalculate.Size = new Size(64, 18);
+			lblCalculate.TabIndex = 3;
+			lblCalculate.Text = "in ms is:";
+			// 
+			// cbTimeUnit
+			// 
+			cbTimeUnit.Anchor = AnchorStyles.None;
+			cbTimeUnit.DropDownStyle = ComboBoxStyle.DropDownList;
+			cbTimeUnit.Font = new Font("Bahnschrift", 11.25F);
+			cbTimeUnit.FormattingEnabled = true;
+			cbTimeUnit.Items.AddRange(new object[] { "minute(s)", "second(s)" });
+			cbTimeUnit.Location = new Point(99, 4);
+			cbTimeUnit.Margin = new Padding(4, 3, 4, 3);
+			cbTimeUnit.Name = "cbTimeUnit";
+			cbTimeUnit.Size = new Size(158, 26);
+			cbTimeUnit.TabIndex = 8;
+			cbTimeUnit.SelectedIndexChanged += cbTimes_ValueChanged;
+			// 
+			// Window
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(570, 157);
+			Controls.Add(tlpWindow);
+			Controls.Add(btnChangeWindowState);
+			FormBorderStyle = FormBorderStyle.FixedSingle;
+			Margin = new Padding(4, 3, 4, 3);
+			MaximizeBox = false;
+			MaximumSize = new Size(586, 196);
+			MinimumSize = new Size(289, 196);
+			Name = "Window";
+			ShowIcon = false;
+			Text = "Autoclicker";
+			Load += Window_Load;
+			((System.ComponentModel.ISupportInitialize)nudInterval).EndInit();
+			pnlUserTimeFrame.ResumeLayout(false);
+			pnlUserTimeFrame.PerformLayout();
+			pnlInteractClicker.ResumeLayout(false);
+			pnlInteractClicker.PerformLayout();
+			tlpWindow.ResumeLayout(false);
+			pnlStandardTimeFrame.ResumeLayout(false);
+			pnlStandardTimeFrame.PerformLayout();
+			pnlTimeCalculator.ResumeLayout(false);
+			pnlTimeCalculator.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)nudInputTime).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnStartStop;
+		private System.Windows.Forms.Label lblInterval;
+		private System.Windows.Forms.Timer tmrClicker;
+		private System.Windows.Forms.NumericUpDown nudInterval;
+		private System.Windows.Forms.Timer tmrKeyDown;
+		private System.Windows.Forms.Label lblHint;
+		private System.Windows.Forms.Panel pnlUserTimeFrame;
+		private System.Windows.Forms.Label lblUserTimeFrame;
+		private System.Windows.Forms.Panel pnlInteractClicker;
+		private System.Windows.Forms.Button btnChangeWindowState;
+		private TableLayoutPanel tlpWindow;
+		private Panel pnlStandardTimeFrame;
+		private Label lblStandardTimeFrame;
+		private Button btn30sec;
+		private Button btn1min;
+		private Button btn5min;
+		private Panel pnlTimeCalculator;
+		private TextBox tbCalcResult;
+		private NumericUpDown nudInputTime;
+		private Label lblCalculate;
+		private ComboBox cbTimeUnit;
+	}
+}
+
---
+++ Window.cs
@@ -0,0 +1,280 @@
+using System.Runtime.InteropServices;
+
+namespace AutoClicker
+{
+	public partial class Window : Form
+	{
+		/// <summary>
+		/// Used for the click event
+		/// </summary>
+		/// <param name="dwFlags"></param>
+		/// <param name="dx"></param>
+		/// <param name="dy"></param>
+		/// <param name="dwData"></param>
+		/// <param name="dwextrainfo"></param>
+		[DllImport("user32.dll")]
+		static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwextrainfo);
+
+		/// <summary>
+		/// Used for de/activating the autoclicker when a key is pressed and window isn't in focus
+		/// </summary>
+		/// <param name="vKey"></param>
+		/// <returns></returns>
+		[DllImport("User32.dll")]
+		static extern short GetAsyncKeyState(Int32 vKey);
+
+
+		bool _clickerIsActive;
+		int _intervalToUse;
+		int _startStopKey;
+		State _windowState;
+		Button[] _timeFrames;
+
+		public Window()
+		{
+			InitializeComponent();
+			
+			_startStopKey = (short)Keys.RShiftKey;
+			_windowState = State.HideExtended;
+			_timeFrames = new Button[3]
+			{
+				btn30sec,
+				btn1min,
+				btn5min
+			};
+		}
+
+		enum MouseEventsFlags
+		{
+			LeftDown = 2,
+			LeftUp = 4,
+		}
+
+		enum State
+		{
+			HideExtended,
+			ShowExtended
+		}
+
+		void Window_Load(object sender, EventArgs e)
+		{
+			tmrKeyDown.Start();
+			_intervalToUse = (int)nudInterval.Value;
+			cbTimeUnit.SelectedIndex = 0;
+			ChangeWindow();
+		}
+
+		void LeftClick(Point p)
+		{
+			mouse_event((int)MouseEventsFlags.LeftDown, p.X, p.Y, 0, 0);
+			mouse_event((int)MouseEventsFlags.LeftUp, p.X, p.Y, 0, 0);
+		}
+
+		void btnStartStop_Click(object sender, EventArgs e)
+		{
+			ChangeActiveState();
+		}
+
+		/// <summary>
+		/// Will start/stop the timer based on if the clicker is active or not.
+		/// </summary>
+		void ChangeActiveState()
+		{
+			_clickerIsActive = !_clickerIsActive;
+			tmrClicker.Interval = _intervalToUse;
+			tmrClicker.Enabled = true;
+
+			if (_clickerIsActive)
+			{
+				btnStartStop.Text = "Stop Autoclicker";
+				tmrClicker.Start();
+			}
+			else
+			{
+				btnStartStop.Text = "Start Autoclicker";
+				tmrClicker.Stop();
+			}
+		}
+
+		/// <summary>
+		/// Event that will trigger the click method if raised.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void timer_Tick(object sender, EventArgs e)
+		{
+			LeftClick(new Point(MousePosition.X, MousePosition.Y));
+		}
+
+		/// <summary>
+		/// Will check if the user has pressed the start/stop key during runtime
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void tmrKeyDown_Tick(object sender, EventArgs e)
+		{
+			short keyStatus = GetAsyncKeyState(_startStopKey);
+
+			if ((keyStatus & 1) == 1)
+			{
+				ChangeActiveState();
+				Thread.Sleep(200);  //prevent enabling/disabling multiple times in a short time}
+			}
+		}
+
+		/// <summary>
+		/// Will set up the timers interval to the desired buttons data
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void btnStandardTimeFrame_Click(object sender, EventArgs e)
+		{
+			Button senderButton = (Button)sender;
+			string[] btnData = senderButton.Text.Split();
+
+			int timeFrame = Convert.ToInt32(btnData[0]);
+			string timeUnit = btnData[1];
+
+			timeUnit = DetermineNextTimeUnit(timeUnit);
+			_intervalToUse = CalculateToMilliSeconds(timeFrame, timeUnit);
+
+			senderButton.Enabled = false;
+			Button[] uninteractedBtns = _timeFrames.Where(btn => btn != senderButton).ToArray();
+			foreach (Button btn in uninteractedBtns)
+			{
+				btn.Enabled = true;
+			}
+		}
+
+		/// <summary>
+		/// Returns the time frame the selected time frame needs to be converted to.
+		/// </summary>
+		/// <param name="timeUnit"></param>
+		/// <returns></returns>
+		string DetermineNextTimeUnit(string timeUnit)
+		{
+			switch (timeUnit)
+			{
+				case "minute(s)":
+				case "min":
+					timeUnit = "sec";
+					break;
+
+				case "second(s)":
+				case "sec":
+					timeUnit = "ms";
+					break;
+			}
+
+			return timeUnit;
+		}
+
+		int CalculateToMilliSeconds(int myTimeFrame, string myNextTimeUnit)
+		{
+			// convert min to sec
+			if (myNextTimeUnit.Equals("sec"))
+			{
+				myTimeFrame *= 60;
+				myNextTimeUnit = "ms";
+			}
+
+			// convert sec to ms
+			if (myNextTimeUnit.Equals("ms"))
+			{
+				myTimeFrame *= 1000;
+			}
+
+			return myTimeFrame;
+		}
+
+		void cbTimes_ValueChanged(object sender, EventArgs e)
+		{
+			CalculateResult();
+		}
+
+		/// <summary>
+		/// Event that handles the nud controls whose value have changed
+		/// Will either stop the clicker if the new click time isnt the same or calculate the given time to ms
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void nudInput_ValueChanged(object sender, EventArgs e)
+		{
+			NumericUpDown nudSender = (NumericUpDown)sender;
+			if (nudSender == nudInputTime)
+			{
+				CalculateResult();
+			}
+			else if (nudSender == nudInterval && _intervalToUse != (int)nudSender.Value)
+			{
+				if (_clickerIsActive)
+				{
+					ChangeActiveState();
+				}
+
+				_intervalToUse = (int)nudSender.Value;
+
+				Button[] toDisable = _timeFrames.Where(b => !b.Enabled).ToArray();
+
+				foreach (Button btn in toDisable)
+				{
+					btn.Enabled = true;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Will update the interval and stop the clicker if the user presses a number key.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void nudInput_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (int.TryParse(e.KeyChar.ToString(), out int result) || (Keys)e.KeyChar == Keys.Back || (Keys)e.KeyChar == Keys.OemBackslash)
+			{
+				nudInput_ValueChanged(sender, e);
+			}
+			else
+			{
+				e.Handled = true;
+			}
+		}
+
+		void CalculateResult()
+		{
+			string nextTimeUnit = DetermineNextTimeUnit(cbTimeUnit.Text);
+			int timeFrame = (int)nudInputTime.Value;
+			int result = CalculateToMilliSeconds(timeFrame, nextTimeUnit);
+			tbCalcResult.Text = result > 0 ? result.ToString() : "ERROR";
+		}
+
+		void btnChangeWindowState_Click(object sender, EventArgs e)
+		{
+			_windowState = _windowState == State.HideExtended ? State.ShowExtended : State.HideExtended;
+			ChangeWindow();
+		}
+
+		/// <summary>
+		/// This will either widen the window to show its other options or reduce it to show its main functions.
+		/// </summary>
+		void ChangeWindow()
+		{
+			AutoSize = true;
+			if (_windowState == State.HideExtended)
+			{
+				//Width = pnlInteractClicker.Width + btnChangeWindowState.Width * 2;
+				tlpWindow.ColumnStyles.Clear();
+				pnlStandardTimeFrame.Visible = false;
+				pnlTimeCalculator.Visible = false;
+				btnChangeWindowState.Text = ">>";
+			}
+			else if (_windowState == State.ShowExtended)
+			{
+				pnlStandardTimeFrame.Visible = true;
+				pnlTimeCalculator.Visible = true;
+				//Width = 470;
+				btnChangeWindowState.Text = "<<";
+			}
+		}
+	}
+}
